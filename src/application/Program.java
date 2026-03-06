ffd2a1e598b7f5fdcdd6267d3af87c566f3d560e
Marcelo Parabocz <macerno@gmail.com>
2026-03-06 15:01:50 -0300

    feat: position class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("This is almost working already!");
+		System.out.println("This is almost working already /s");
+
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
